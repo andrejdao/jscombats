01bbb690b36ccd4b1fdfe0735f73d2cc87ead940
root <superdoctor666@gmail.com>
2019-06-11 19:46:25 +0300

    second commit

---
+++ build/precache-manifest.5db43da9017e61ddd72da66808b52599.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa18ee98260ee705a8a19f72e596315e",
+    "url": "/static/media/amulet.fa18ee98.svg"
+  },
+  {
+    "revision": "da91459f906a6cf6b552",
+    "url": "/static/css/main.667fb076.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6a871ca8e9685da0a43",
+    "url": "/static/js/2.f6e24d44.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "38b363f107ad04ceae4ec1c9f262497d",
+    "url": "/static/media/empty.38b363f1.svg"
+  },
+  {
+    "revision": "9c914fd679a83a9f2ad845b164ce51d1",
+    "url": "/static/media/shield.9c914fd6.svg"
+  },
+  {
+    "revision": "63f6659efbcf1127d799fa65b54c54f8",
+    "url": "/static/media/sword.63f6659e.svg"
+  },
+  {
+    "revision": "da91459f906a6cf6b552",
+    "url": "/static/js/main.40482028.chunk.js"
+  },
+  {
+    "revision": "381fe8d9003b9ab15c1874c2e62f033c",
+    "url": "/static/media/head.381fe8d9.svg"
+  },
+  {
+    "revision": "a059379ec9bccde67467d5fe0248353b",
+    "url": "/static/media/ring.a059379e.svg"
+  },
+  {
+    "revision": "817e50b286d478012b85981c31c484f6",
+    "url": "/static/media/armor.817e50b2.svg"
+  },
+  {
+    "revision": "3f39d7387a2e77b1af8e9f6ce72e6e45",
+    "url": "/static/media/belt.3f39d738.svg"
+  },
+  {
+    "revision": "01de6148951c14ab226292e5716ae6bb",
+    "url": "/static/media/boots.01de6148.svg"
+  },
+  {
+    "revision": "5ef3a6f1b27247bc1c17d8d34e233fef",
+    "url": "/static/media/gauntlet.5ef3a6f1.svg"
+  },
+  {
+    "revision": "e5f2b4daa3d8fa4ba8de7df133cf4f43",
+    "url": "/index.html"
+  }
+];
